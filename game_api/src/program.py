afc0efae099aa1199c4310396a79eef9253cfced
joris023 <144436748+joris023@users.noreply.github.com>
2026-03-25 00:13:58 +0100

    make docker compose with db

---
+++ game_api/src/program.py
@@ -0,0 +1,7 @@
+print("This is the Game API")
+
+import numpy as np
+
+test = np.array([1, 2, 3])
+
+print(test)
--- src/pannenkoek/main.py
+++
@@ -1,7 +0,0 @@
-
-def test() -> None:
-    print("hello")
-
-
-if __name__ == "__main__":
-    test()
